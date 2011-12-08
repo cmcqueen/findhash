426069320ae6e3c1e2da48194de7f077ab51d373
Craig McQueen <craig.mcqueen@beamcommunications.com>
2011-12-08 14:20:54 +1100

    Initial files.

---
+++ findhash.py
@@ -0,0 +1,73 @@
+
+from collections import defaultdict
+
+def shift_vector_gen(smin, smax, n):
+    vector = list(xrange(smin, smin + n))
+    while True:
+        yield tuple(vector)
+        i = n - 1
+        while True:
+            for j in xrange(i, n):
+                if j == i:
+                    vector[j] += 1
+                else:
+                    vector[j] = vector[j - 1] + 1
+                if (vector[j] >= smax):
+                    if i == 0:
+                        return
+                    else:
+                        i -= 1
+                        break
+            else:
+                break
+
+def shift_functions_gen(smin, smax, n, hash_size):
+    for shift_vector in shift_vector_gen(smin, smax, n):
+        def make_hash_function(shift_vector, hash_size):
+            def hash_function(value):
+                out_value = 0
+                for shift_value in shift_vector:
+                    if shift_value >= 0:
+                        out_value ^= value << shift_value
+                    else:
+                        out_value ^= value >> (-shift_value)
+                return out_value % hash_size
+            hash_function.__doc__ = "Shift function with vectors {0}, hash size {1}".format(shift_vector, hash_size)
+            hash_function.hash_size = hash_size
+            return hash_function
+        yield make_hash_function(shift_vector, hash_size)
+
+def check_hash(o, hash_function):
+    hash_output_count = defaultdict(int)
+    hash_size = 0
+    num_collisions = 0
+    max_collisions = 0
+    for i in o:
+        hash = hash_function(i)
+        hash_output_count[hash] += 1
+    return (hash_size, num_collisions, max_collisions, hash_function.hash_size)
+
+def find_hash(o, hash_functions, hash_size):
+    # best_size_result in form (hash_size, num_collisions, f_hash_size, f)
+    best_size_result = { 'size' : 1000000000, 'collisions' : 1000000000, 'f_hash_size' : None, 'f' : None }
+    best_collision_result = { 'size' : 1000000000, 'collisions' : 1000000000, 'f_hash_size' : None, 'f' : None }
+    for f in hash_functions:
+        (hash_size, num_collisions, max_collisions, f_hash_size) = check_hash(o, f)
+        if ((hash_size < best_result['size']) or
+            ((hash_size == best_result['size']) and (num_collisions < best_result['collisions']))):
+                best_size_result = { 'size' : hash_size, 'collisions' : num_collisions, 'f_hash_size' : f_hash_size, 'f': f } 
+        if ((result['collisions'] < best_result['collisions']) or
+            ((result['collisions'] == best_result['collisions']) and (hash_size < best_result['size']))):
+                best_collision_result = { 'size' : hash_size, 'collisions' : num_collisions, 'f_hash_size' : f_hash_size, 'f': f } 
+    return (best_size_result, best_collision_result)
+
+
+if __name__ == "__main__":
+    for f in shift_functions_gen(-3, 6, 3, 16):
+        print f.__doc__
+    print
+    
+    f = list(shift_functions_gen(-3, 6, 3, 16))[7]
+    print f.__doc__
+    for i in xrange(16):
+        print f(i)
---
+++ gsm0338_parse.py
@@ -0,0 +1,89 @@
+"""
+Python 2.x program
+
+Parse GSM0338.TXT file to generate C code for SMS handling of GSM 03.38 which
+specifies SMS 7-bit alphabet. C code tables are needed for:
+
+* Unicode to 7-bit alphabet mapping
+* 7-bit alphabet to Unicode mapping
+
+GSM0338.TXT file is from Unicode web site:
+    http://unicode.org/Public/MAPPINGS/ETSI/GSM0338.TXT
+"""
+
+# Standard Python modules
+import re
+from pprint import pprint
+from collections import defaultdict
+
+GSM0338_FILENAME = 'GSM0338.TXT'
+
+# Regular expression for mapping table entries. Matches 7-bit code values, Unicode value, comment
+line_re = re.compile(ur'^\#?0x([0-9a-fA-F]{2})([0-9a-fA-F]*)\s+0x([0-9a-fA-F]{4})\s+\#\s+(.*)$')
+
+def parse_line(line):
+    match = line_re.match(line)
+    if match:
+        #print match.groups()
+        gsm1, gsm2, uni, comment = match.groups()
+        gsm1 = int(gsm1, 16)
+        if gsm2:
+            gsm2 = int(gsm2, 16)
+        else:
+            gsm2 = 0
+        uni = int(uni, 16)
+        return (gsm1, gsm2, uni, comment)
+
+def read_data(include_commented = False):
+
+    data = []
+
+    with open(GSM0338_FILENAME) as f:
+        for line in f:
+            if not include_commented and line[0] == '#':
+                continue
+            parsed = parse_line(line)
+            if parsed:
+                data.append(parsed)
+    return data
+
+def read_unicode_data(include_commented = True):
+
+    data = []
+
+    for row in read_data(include_commented):
+        (gsm1, gsm2, uni, comment) = row
+        if gsm2:
+            if gsm1 != 0x1b:
+                raise Exception("Unexpected gsm1 value")
+            gsm1 = gsm2 + 128
+        data.append((uni, gsm1, comment))
+    return data
+
+def read_escape_data():
+    data = []
+    
+    for row in read_data():
+        (gsm1, gsm2, uni, comment) = row
+        if gsm2:
+            if gsm1 != 0x1b:
+                raise Exception("Unexpected gsm1 value")
+            data.append((gsm2, uni, comment))
+    return data
+
+def read_7bit_data():
+    data = []
+
+    for row in read_data():
+        (gsm1, gsm2, uni, comment) = row
+        if not gsm2 and gsm1 != uni:
+            data.append((gsm1, uni, comment))
+    return data
+
+def make_7bit_to_unicode_table_old():
+
+    #pprint(read_data())
+    pprint(read_7bit_data())
+
+if __name__ == "__main__":
+    make_7bit_to_unicode_table_old()
